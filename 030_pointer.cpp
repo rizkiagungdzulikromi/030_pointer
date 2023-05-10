4047f5f2fecc3d160f5e5e5384344251b83d1767
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-10 15:09:23 +0700

    deklarasi method

--- 030_pointer.cpp
+++ 030_pointer.cpp
@@ -6,23 +6,3 @@ public:
 	int nim;
 	void showNim(); //deklarasi method
 };
-
-void mahasiswa::showNim() {           //implementasi method diluar class
-	cout << "No Induk = " << nim << endl;
-}
-
-int main()
-{
-	mahasiswa mhs{ 1 };          //object mhs
-	mhs.showNim();               //member Acces Operator
-
-	mahasiswa& ref = mhs;
-	ref.nim = 2;
-	mhs.showNim();
-
-	mahasiswa* pMhs = &mhs;
-	pMhs->nim = 3;
-	mhs.showNim();
-	system("pause");
-	return 0;
-}
