f67bd2c83fbd357a59f397628550adbbea1a2850
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-10 16:10:10 +0700

    membuat program di luar class mahasiswa

--- friendclass2/friendclass2.cpp
+++ friendclass2/friendclass2.cpp
@@ -9,3 +9,15 @@ public:
 	friend void setNama(mahasiswa& a, string);
 };
 
+void setNama(mahasiswa& a, string pNama) {
+	a.nama = pNama;
+	cout << "nama: " << a.nama;
+}
+int main()
+{
+	mahasiswa mhs;
+	setNama(mhs, "joko kumat");
+	cout << endl;
+	system("pause");
+	return 0;
+}
