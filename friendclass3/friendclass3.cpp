4e4626a6fefbdcfcbe26de34c4141d34bf976881
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-10 16:20:56 +0700

    membuat program class manusia dan class pelajar

---
+++ friendclass3/friendclass3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+
+class manusia {
+public:
+	void showNilaiPelajar(pelajar&);
+};
+
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
