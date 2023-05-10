6e9b52f69e27271392b2645ac832b96356102822
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-10 15:11:59 +0700

    object mhs

--- 030_pointer.cpp
+++ 030_pointer.cpp
@@ -11,3 +11,18 @@ void mahasiswa::showNim() {           //implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };          //object mhs
+	mhs.showNim();               //member Acces Operator
+
+	mahasiswa& ref = mhs;
+	ref.nim = 2;
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
